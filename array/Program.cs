8aa36d8191a1d225cbe940d6f58d86fd1cf4be73
JP Goyetche <julien-pierregoyetche@Julien-Pierres-MacBook-Air.local>
2026-04-06 22:16:01 -0400

    array-project

---
+++ array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Initialize and populate the array with books
+        string[] favoriteBooks = {
+            "To Kill a Mockingbird",
+            "Nous, les dieux",
+            "Dorian grey",
+            "Invincible",
+            "The Odyssey",
+            "troisieme humanite"
+        };
+
+        Console.WriteLine("📚 My Favorite Books:\n");
+
+        
+        foreach (string book in favoriteBooks)
+        {
+            Console.WriteLine("- " + book);
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
