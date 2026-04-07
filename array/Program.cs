37d57b34f5a32af7b5e06fff508afd4e5c88268c
jpnewcal <jp-nat-cnc@hotmail.fr>
2026-04-06 23:00:28 -0400

    Remove comment on array initialization
    Removed comment about array initialization.

--- array/Program.cs
+++ array/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Initialize and populate the array with books
         string[] favoriteBooks = {
             "Nous, les dieux",
             "Dorian grey",
